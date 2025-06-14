4d8d26fe3f9375b4312fced7cede77ef81a1068f
Mupenzi Herve <mupenziherve04@gmail.com>
2025-06-14 13:34:37 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Hero from './components/Hero';
+import Service from './components/Service';
+import Tours from './components/Tours';
+import Footer from './components/Footer';
+
+
+function App() {
+ 
+  return (
+    <>
+   <Navbar/>
+   <About/>
+    <Hero/>
+    <Service/>
+    <Tours/>
+    <Footer/>
+
+
+    </>
+  
+  );
+  }
+
+export default App;
