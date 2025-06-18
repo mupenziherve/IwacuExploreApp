7c5fc0ce2ad52196ad0b738822dc7b938e1a5435
Mupenzi Herve <mupenziherve04@gmail.com>
2025-06-18 20:24:30 +0000

    Fixed ESLint warning in Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,17 +18,18 @@ const Footer = () => {
        
       </ul>
       <ul className="footer-icons">
-        {socialLinks.map((link)=>{
-          const {id, href, icon} = link
-          return (
-            <li>
-          <a href={href} target="_blank" className="footer-icon" rel="noreferrer">
-            <i className={icon} ></i></a>
-        </li>
+      {socialLinks.map((link) => {
+  const { id, href, icon } = link;
+  return (
+    <li key={id}>
+      <a href={href} target="_blank" className="footer-icon" rel="noreferrer">
+        <i className={icon}></i>
+      </a>
+    </li>
+  );
+})}
 
-          );
 
-        })}
         
     
       </ul>
